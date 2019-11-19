a49d8eae85b957accc388c9aea93d922d2635923
Annonymus <marcotereh97@gmail.com>
2019-11-19 10:03:31 +0100

    set up initial node project using express

---
+++ app.js
@@ -0,0 +1,27 @@
+// We're not using the commented modules but may want to later on.
+const express = require('express');
+// const path = require('path');
+const logger = require('morgan');
+// const bodyParser = require('body-parser');
+// const kleiDust = require('klei-dust');
+// const dustjsLinkedin = require('dustjs-linkedin');
+
+const app = express();
+
+//configure app
+app.use(logger('dev'));
+// app.use(bodyParser.json({strict: false}));
+// app.use(bodyParser.text());
+// app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/public'));
+
+// app.set('view engine', 'dust');
+// app.set('views', __dirname + '/views');
+// app.engine('dust', kleiDust.dust);
+
+
+// Initialize routers here
+const routers = require(__dirname + '/routes/routers');
+app.use('/', routers.root);
+
+app.listen(3000);
