3a45aa2d4620bdda225029ce48defc90e6b836fb
Annonymus <marcotereh97@gmail.com>
2019-11-21 08:02:38 +0100

    add a rudimentary engine and instructions on its use

---
+++ engine/engine.js
@@ -0,0 +1,31 @@
+const TICK_RATE = 30;
+const WORLD_WIDTH = 100;
+const WORLD_HEIGHT = 100;
+
+module.exports.TICK_RATE = TICK_RATE;
+module.exports.WORLD_WIDTH = TICK_RATE;
+module.exports.WORLD_HEIGHT = TICK_RATE;
+
+let timer;
+let tick_num = 0;
+
+// Starts the engine
+function init() {
+    function tick_repeater() {
+        // Don't use setinterval to prevent skipping ticks.
+        timer = setTimeout(tick_repeater, 1000/TICK_RATE);
+        tick();
+    }
+    tick_repeater();
+}
+
+// This function must be completely synchronous.
+function tick() {
+    tick_num++;
+}
+
+function shutdown() {
+    if (timer) {
+        clearTimeout(timer);
+    }
+}
