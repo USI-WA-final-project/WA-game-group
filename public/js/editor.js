de210cb732c49fe5f3a31c3228b779d979744f33
tommi27 <tommi99@hotmail.it>
2019-11-28 11:41:40 +0100

    fixed syntax error, modified editor bar

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,126 +1,73 @@
 class Editor {
-    constructor(statusNum) {
-        this.canvas = document.getElementById('canvas');
-        this.ctx = this.canvas.getContext('2d');
+    constructor(statusNum, player) {
+        this.player = player;
+        this.currentCell = this.player[0];
+        this.type = undefined;
 
-        // will draw the current icon at the mouse position
-        // To be discussed as drawing on the client might be too heavy
         switch (statusNum) {
             case 0:
-            this.start();
                 break;
             case 1:
-                this.start();
-                this.canvas.addEventListener('mousemove', (e) => {
-                    this.drawCell(e);
-                });
+                this.type = 0;
                 break;
             case 2:
-                this.start();
-                this.canvas.addEventListener('mousemove', (e) => {
-                    this.drawSpike(e);
-                });
+                this.type = 1;
                 break;
             case 3:
-                this.start();
-                this.canvas.addEventListener('mousemove', (e) => {
-                    this.drawShield(e);
-                });
+                this.type = 2;
                 break;
             case 4:
-                this.start();
-                this.canvas.addEventListener('mousemove', (e) => {
-                    this.drawBounce(e);
-                });
+                this.type = 3;
                 break;
         }
     }
 
-    // clear function to remove previous event listeners
-    start() {
-        try {
-            console.log('CLEARING LISTENERS');
-            this.canvas.removeEventListener('mousemove', (e) => {
-                 this.drawCell(e);
-             });
-            this.canvas.removeEventListener('mousemove', (e) => {
-                this.drawSpike(e);
-            });
-            this.canvas.removeEventListener('mousemove', (e) => {
-                this.drawShield(e);
-            });
-            this.canvas.removeEventListener('mousemove', (e) => {
-                this.drawBounce(e);
-            });
-        } catch(e) {
-            console.error(e);
-            throw new Error(e);
-        }
-    }
+    findFaces() {
 
-    // draws image at cursor position
-    drawCell(e) {
-        console.log('DRAWING CELL');
-        const image = document.getElementById('cell-edit');
-        this.ctx.drawImage(image, e.offsetX - 10, e.offsetY - 10);
-    }
-
-    drawSpike(e) {
-        console.log('DRAWING SPIKE');
-        const image = document.getElementById('spike-edit');
-        this.ctx.drawImage(image, e.offsetX - 10, e.offsetY - 10);
-    }
-
-    drawShield(e) {
-        console.log('DRAWING SHIELD');
-        const image = document.getElementById('shield-edit');
-        this.ctx.drawImage(image, e.offsetX - 10, e.offsetY - 10);
-    }
-
-    drawBounce(e) {
-        console.log('DRAWING BOUNCE');
-        const image = document.getElementById('bounce-edit');
-        this.ctx.drawImage(image, e.offsetX - 10, e.offsetY - 10);
     }
 
     // searches the player object for the next item of type cell starting from
     // the index startIndex
-    findNextCell(player, startIndex) {
-        for (let i = startIndex; i < player.components.length; i++) {
-            if (player.components[i].type == 0) {
-                return player.components[i];
+    findNextCell(player) {
+        let current;
+        for (let i = player.indexOf(currentCell); i < player.length; i++) {
+            if (player[i].type == 0) {
+                current = player[i];
+                this.currentCell = current;
+                return current;
             }
         }
         // if there is no next cell, the last element of the array of type cell
         // will be returned instead
-        return findPrevCell(player, player.components.length - 1)
+        return findPrevCell(player);
     }
 
     // searches the player object for the previous item of type cell starting
     // from the index startIndex
-    findPrevCell(player, startIndex) {
-        for (let i = startIndex; i >= 0; i--) {
-            if (player.components[i].type == 0) {
-                return player.components[i];
+    findPrevCell(player) {
+        let current;
+        for (let i = player.indexOf(currentCell); i >= 0; i--) {
+            if (player[i].type == 0) {
+                current = player[i];
+                this.currentCell = current;
+                return current;
             }
         }
         // the function should never actually terminate outside of the loop
         // as the first element of the components will always be the
         // core cell of the player
+        current = player.components[0];
         return player.components[0];
     }
 
     /*
-     * Adds a new cell of type 'type', at face 'face' of cell 'cell' to the
-     * player object
+     * Adds a new cell of type 'this.type', at face 'face' of 'this.currentCell'
+     * to 'this.player'
      * @param {player} the player object
      * @param {face} the face number given by the player. Number from 1 to 6
-     * @param {type} the type passed into the editor object
-     * @param {cell} the cell selected by the user navigating the object through
-     * the findNextCell and findPrevCell functions
      * @returns the updated player object
      */
-    setCell(player, face, type, cell) {
+    setCell(player, face) {
         newCell = {
             "type": type,
             "faces": [-1, -1, -1, -1, -1, -1]
@@ -129,32 +76,29 @@ class Editor {
         // number between 1 and 6 while the indexes go from 0 to 5
 
         // connect newCell face to cell face
-        newCell.faces[oppositeFace(face) - 1] = player.components.indexOf(cell);
-        player.components.push(newCell);
+        newCell.faces[oppositeFace(face) - 1] = this.player.indexOf(cell);
+        this.player.push(newCell);
         // connect cell face to newCell face
-        cell.faces[face - 1] = player.components.indexOf(newCell);
+        this.currentCell.faces[face - 1] = player.indexOf(newCell);
 
-        return player;
+        return this.player;
     }
 
     /*
-     * Adds a new leaf element of type 'type', at face 'face' of cell 'cell' to
-     * the player object
+     * Adds a new leaf element of type 'this.type', at face 'face' of
+     * 'this.currentCell' to 'this.player'
      * @param {player} the player object
      * @param {face} the face number given by the player. Number from 1 to 6
-     * @param {type} the type passed into the editor object
-     * @param {cell} the cell selected by the user navigating the object through
-     * the findNextCell and findPrevCell functions
      * @returns the updated player object
      */
-    setElement(player, face, type, cell) {
+    setElement(player, face) {
         newElement = {
-            "type": type;
+            "type": this.type
         };
-        player.components.push(newElement);
-        cell.faces[face - 1] = player.components.indexOf(newElement);
+        player.push(newElement);
+        cell.faces[face - 1] = player.indexOf(newElement);
 
-        return player;
+        return this.player;
     }
 
     // given the number of a face, returns the opposite face
