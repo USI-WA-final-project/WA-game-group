abbb6710ee92db388879835924113ae8f753e81b
Stefano Taillefert <stefano.taille@gmail.com>
2019-11-29 10:20:36 +0100

    added JSdoc for database API

--- models/Player.js
+++ models/Player.js
@@ -4,13 +4,15 @@ const Player = exports.Player = new mongoose.Schema({
     id: { type: Number, required: true },
     color: { type: Number, required: true },
     username: { type: String, required: true },
-    dateStarted: { type: Date, default: Date.now },
-    dateEnded: { type: Date, default: null },
-    active: { type: Boolean, default: true },
     spawnPos: { 
         x: { type: Number, required: true },
         y: { type: Number, required: true }
-    }
+    },
+    //Optional fields
+    dateStarted: { type: Date, default: Date.now },
+    dateEnded: { type: Date, default: null },
+    active: { type: Boolean, default: true },
+    score: { type: Number, default: 0 }    
 });
 
 mongoose.model('Player', Player);
