f3319b042d374becbc51b25f97cb6a9e2cd03ae6
tommi27 <tommi99@hotmail.it>
2019-12-02 11:24:08 +0100

    updated editor

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,8 +1,9 @@
 class Editor {
     constructor(player) {
-        this.counter = 0;
         this.player = player;
-        this.face = undefined;
+        this.centers = undefined;
+        this.focus = undefined;
+        this.counter = 0;
         if (!this.player) {
             this.currentCell = undefined;
         } else {
@@ -10,10 +11,6 @@ class Editor {
         }
     }
 
-    /*
-     * Searches the player for the next item of type cell starting from
-     * this.counter
-     */
     findNextCell() {
         let current;
         for (let i = this.counter; i < this.player.length; i++) {
@@ -27,10 +24,6 @@ class Editor {
         }
     }
 
-    /*
-     * Searches the player for the previous item of type cell starting from
-     * this.counter
-     */
     findPrevCell() {
         let current;
         for (let i = this.counter; i >= 0; i--) {
@@ -46,6 +39,79 @@ class Editor {
         current = this.player.components[0];
         this.currentCell = current;
         this.counter = 0;
+    }
+
+    /*
+     * Updates this.counter to the value of the cell holding the nearest center
+     * to this.focus and finds the face closest to this.focus
+     * returns {number} face - The number of the face closest to this.focus.
+     * Must be between 0 and 5
+     */
+    findFace() {
+        if (!this.centers || !this.focus) {
+            console.log('focus', this.focus);
+            console.log('center array', this.centers);
+            throw "At least one paramater is undefined";
+        }
+
+        let face;
+        let bestDistance = Infinity;
+        let counter = 0;
+        let closest;
+
+        for (let i = 0; i < this.centers.length; i++) {
+            if (this.centers[i] != -1) {
+                counter++;
+                let currentDist = calcDistance(this.focus, this.centers[i]);
+                if (currentDist < bestDistance) {
+                    bestDistance = currentDist;
+                    closest = this.centers[i];
+                }
+            }
+        }
+        this.counter = counter;
+        face = checkFace(this.focus, closest);
+        return face;
+    }
+
+    /*
+     * Finds the distance between two points
+     * @param start first point
+     * @param end second point
+     * @returns {number} The distance between the two points
+     */
+    calcDistance(start, end) {
+        return Math.sqrt(
+            Math.pow(end.x - start.x, 2) +
+            Math.pow(end.y - start.y, 2)
+        );
+    }
+
+    /*
+     * Checks the focus coordinates and the center coordinates and finds the
+     * face closest to focus
+     * @param focus - coordinates at mouseclick
+     * @param center - coordinates of the center closest to focus
+     * @returns {number} The number of the face between 0 and 5
+     */
+    checkFace(focus, center) {
+        if (focus.y >= center.y) {
+            if (focus.x >= center.x + 8) {
+                return 2;
+            } else if (focus.x < center.x + 8 && focus.x > center.x - 8) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (focus.x >= center.x + 8) {
+                return 3;
+            } else if (focus.x < center.x + 8 && focus.x > center.x - 8) {
+                return 4;
+            } else {
+                return 5;
+            }
+        }
     }
 }
 
