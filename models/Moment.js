56c5e45433ffff01b99c2e9b0b69f58047c2ec7c
Gianmarco De Vita <devitg@usi.ch>
2019-12-06 21:57:28 +0100

    Improved style

--- models/Moment.js
+++ models/Moment.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const Moment = exports.Moment = new mongoose.Schema({
-    name: { type: String, default: "Epic moment " + Date.now() },
+    name: { type: String, default: "Epic moment " + JSON.stringify(new Date()) },
     date: { type: Date, default: Date.now() },
     src: { type: String, required: true},
     uploaded: { type: Array, default: [-1, -1] },
