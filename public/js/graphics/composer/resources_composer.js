de585c085b1329421a36801ba7223f6d33426d55
britesma <andreabritesma@gmail.com>
2019-12-13 09:29:40 +0100

    added check resources on button editor, resource size

--- public/js/graphics/composer/resources_composer.js
+++ public/js/graphics/composer/resources_composer.js
@@ -21,10 +21,14 @@ class ResourcesComposer {
         //console.log(resources);
         resources.forEach((it) => {
             this.ctx.beginPath();
-            this.ctx.fillStyle = RESOURCES_FILL_COLOR;
+            let sat = 1;
+            if (it.amount > 235) {
+                sat = it.amount - 235 / 500;
+            }
+            this.ctx.fillStyle = "rgba(238, 226, 221, 1)";
             this.ctx.strokeStyle = RESOURCES_STROKE_COLOR;
-
-            this.ctx.arc(originPoint.x + it.position.x, originPoint.y + it.position.y, 12, 0, 360);
+            let radius = Math.floor(it.amount/10)+5;
+            this.ctx.arc(originPoint.x + it.position.x, originPoint.y + it.position.y, radius, 0, 360);
 
             this.ctx.fill();
             this.ctx.stroke();
